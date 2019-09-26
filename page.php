b3634a8d1c202678cf2d0da1bdfd203cce6fed43
cadetto <cadettoc96@gmail.com>
2019-09-26 16:26:47 -0400

    Design simple single page layout

--- page.php
+++ page.php
@@ -1,5 +1,32 @@
 <?php get_header()?>
 
-   <h1>Prueba de pagina single</h1>
+<?php
+    if(have_posts()):
+        the_post()
+?>
+
+    <div class="single-page-thumbnail"
+        style="background-image:
+            linear-gradient(
+                rgba(0, 0, 0, 0.6),
+                rgba(0, 0, 0, 0.6)
+            ),
+        url('<?= get_the_post_thumbnail_url() ?>')"
+    >
+        <div class="page-title">
+            <h1><?= the_title() ?></h1>
+        </div>
+    </div>
+
+    <div class="single-page-wrapper">
+        <div class="single-page-content">
+
+            <p><?= the_content() ?></p>
+        </div>
+
+    </div>
+
+
+<?php endif ?>
 
 <?php get_footer() ?>
